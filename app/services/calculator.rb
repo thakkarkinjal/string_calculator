e514b318fbdbce6f7a1afb619edd81fccfa46c64
kinjal <kinjalthakkar1619@gmail.com>
2025-01-24 15:38:03 +0530

    Implement logic for delimeters

--- app/services/calculator.rb
+++ app/services/calculator.rb
@@ -2,6 +2,11 @@ class Calculator
 
 	def add(numbers)
 		return 0 if numbers.length == 0
+		if numbers.start_with?("//")
+			if numbers.include?(";")
+				numbers = numbers.gsub(";", ",")
+			end
+		end
 		numbers.gsub(/\n/, ',').split(',').map(&:to_i).sum
 	end
 end
