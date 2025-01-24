e56119c3513ef6c6cf87ae2f8765e6d1ca74e710
kinjal <kinjalthakkar1619@gmail.com>
2025-01-24 18:49:32 +0530

    Raise an exception for negative numbers

--- app/services/calculator.rb
+++ app/services/calculator.rb
@@ -3,10 +3,17 @@ class Calculator
 	def add(numbers)
 		return 0 if numbers.length == 0
 		delimeter = ","
+
+		# handles any custom delimeter
 		if numbers.start_with?("//")
 			delimeter = numbers.match(/\/\/(.)\n/)[1]
 		end
 		numbers = numbers.gsub(/\/\/(.)\n/, "")
-		numbers.gsub("\n", delimeter).split(delimeter).map(&:to_i).sum
+		numbers = numbers.gsub("\n", delimeter).split(delimeter).map(&:to_i)
+
+		# throws an exception for negative numbers
+		negatives = numbers.select(&:negative?)
+		raise "Negatives not allowed: #{negatives.join(', ')}" if negatives.present?
+		numbers.sum
 	end
 end
