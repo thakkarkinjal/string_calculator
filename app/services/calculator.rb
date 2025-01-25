669199d328cf9dd11ff4bf88e78b60b34cc2387b
kinjal <kinjalthakkar1619@gmail.com>
2025-01-25 19:04:11 +0530

    implemented multiple delemeters and refactor code

--- app/services/calculator.rb
+++ app/services/calculator.rb
@@ -6,19 +6,25 @@ class Calculator
 
 		# handles any custom delimeter
 		if numbers.start_with?("//")
-			if numbers.start_with?("//[")
-				delimeter = numbers.match(%r{//\[(.*?)\]})[1]
-				numbers = numbers.split("\n", 2)[1]
-			else
-				delimeter = numbers.match(%r{//(.)\n})[1]
-			end
+			delimeter = find_delimeters(numbers)
+			numbers = numbers.split("\n")[1]
 		end
-		numbers = numbers.gsub(/\/\/(.)\n/, "")
-		numbers = numbers.gsub("\n", delimeter).split(delimeter).map(&:to_i)
+		numbers = numbers.split(/#{delimeter}/).map(&:to_i)
 
 		# throws an exception for negative numbers
 		negatives = numbers.select(&:negative?)
 		raise "Negatives not allowed: #{negatives.join(', ')}" if negatives.present?
 		numbers.reject{ |num| num > 1000 }.sum
+	end
+
+	private
+
+	def find_delimeters(numbers) # find custom delimeter
+		if numbers.start_with?("//[")
+			delimeters = numbers.scan(%r{\[(.*?)\]}).flatten.map { |d| Regexp.escape(d) }
+			delimeter = delimeters.join("|")
+		else
+			delimeter = Regexp.escape(numbers[2])
+		end
 	end
 end
